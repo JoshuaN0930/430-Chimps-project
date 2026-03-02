18e69539f7672629dd5833bdb5e4d728c7bdcb85
JoshuaN0930 <joshua.guzman.7@my.csun.edu>
2026-03-01 19:37:42 -0800

    added all operators

--- src/lexer/token.py
+++ src/lexer/token.py
@@ -4,6 +4,13 @@ from dataclasses import dataclass
 class TokenType(Enum):
     #op
     Plus = auto()
+    Minus = auto()
+    Star = auto()
+    Division = auto()
+    LessThan = auto()
+    EQ = auto()
+    NEQ = auto()
+
 
     #symbols
     LParen = auto()
@@ -13,22 +20,35 @@ class TokenType(Enum):
     VARDEC = auto()
     INT = auto()
     ASSIGN = auto()
+    PRINTLN = auto()
     IDENTIFIER = auto()
     INTEGER = auto()
+
 
 
 class Symbol:
     SINGLE = {
         '+' : TokenType.Plus,
         '(' : TokenType.LParen,
-        ')' : TokenType.RParen
+        ')' : TokenType.RParen,
+        '-' :TokenType.Minus,
+        '*' :TokenType.Star,
+        '/' :TokenType.Division,
+        '<' :TokenType.LessThan
+
+    }
+
+    DOUBLE = {
+        '==' : TokenType.EQ,
+        '!=' : TokenType.NEQ
     }
 
 class Keyword:
     WORDS = {
         'int' :TokenType.INT,
         'vardec' :TokenType.VARDEC,
-        'assign' :TokenType.ASSIGN
+        'assign' :TokenType.ASSIGN,
+        'println' :TokenType.PRINTLN
 
     }
 
@@ -44,5 +64,5 @@ class Token:
         if self.type in (TokenType.IDENTIFIER, TokenType.INTEGER):
             return f"{self.type.name}({self.value})"
         if self.value in Keyword.WORDS:
-            return f"{self.type.name}_Token"
-        return f"{self.type.name}Token"
+            return f"{self.type.name}"
+        return f"{self.type.name}"
