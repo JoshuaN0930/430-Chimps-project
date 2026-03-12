95701d471a2fc2be0ba650c9733eb5f81b87c59a
bastid486 <jbastidasgonzale@students.avc.edu>
2026-03-12 16:11:53 -0700

    Added tests for single symbols

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,4 +1,5 @@
-from src.lexer.token import TokenType
+import pytest
+from src.lexer.token import TokenType, Symbol, Keyword
 from src.lexer.tokenizer import tokenize
 
 #test for '-'
@@ -35,3 +36,12 @@ def test_integerLit():
     assert len(tokens) == 1
     assert tokens[0].type == TokenType.INTEGER
     assert tokens[0].value == 34
+
+# Runs a test on each single symbol found in token.py
+@pytest.mark.parametrize("input_symbol, expected", Symbol.SINGLE.items())
+def test_single_symbols(input_symbol, expected):
+    tokens = tokenize(input_symbol)
+    assert len(tokens) == 1
+    assert tokens[0].type == expected
+    assert tokens[0].value == input_symbol
+    assert tokens[0].line == 1
