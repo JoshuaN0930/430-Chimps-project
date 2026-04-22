ba4df233ca7ef95ecbd655fba2ae4c7fa6513add
bastid486 <jbastidasgonzale@students.avc.edu>
2026-04-22 15:35:12 -0700

    Added documentation to tokenizer.py

--- src/lexer/tokenizer.py
+++ src/lexer/tokenizer.py
@@ -1,7 +1,25 @@
 from .token import Token, TokenType, Symbol, Keyword
 
 
-def tokenize(source: str) -> list [Token]:
+def tokenize(source: str) -> list[Token]:
+    """
+    Convert source code into a list of tokens.
+
+    Parameters
+    ----------
+    source : str
+        The input source code to scan.
+
+    Returns
+    -------
+    list[Token]
+        A list of tokens produced from the source code.
+
+    Raises
+    ------
+    ValueError
+        Raised when an unexpected character is found in the source.
+    """
     tokens = []
     i = 0
     line = 1
@@ -9,32 +27,31 @@ def tokenize(source: str) -> list [Token]:
     while i < len(source):
         ch = source[i]
 
-        #check if current char is a newline, incremetn line coutner and move on
+        #check if current char is a newline, increment line counter and move on
         if ch == '\n':
             line += 1
             i += 1
             continue
 
-        #check if current char is whitespace (space and tab) and skip it 
+        #check if current char is whitespace (space and tab) and skip it
         if ch.isspace():
             i += 1
             continue
 
-        #check for double char sybols like == and !=
+        #check for double char symbols like == and !=
         twoCh = source[i:i+2]
         if twoCh in Symbol.DOUBLE:
             tokens.append(Token(Symbol.DOUBLE[twoCh], twoCh, line))
             i += 2
             continue
 
-        #chekng single-char symbols look it in symbol.single 
+        #checking single-char symbols look it in symbol.single
         elif ch in Symbol.SINGLE:
             tokens.append(Token(Symbol.SINGLE[ch], ch, line))
             i += 1
             continue
 
-
-        #handel keywords (keywords and identifiers)
+        #handle keywords (keywords and identifiers)
         if ch.isalpha() or ch == '_':
             start = i
             while i < len(source) and (source[i].isalnum() or source[i] == "_"):
@@ -44,7 +61,7 @@ def tokenize(source: str) -> list [Token]:
             tokens.append(Token(token_type, word, line))
             continue
 
-        #handel integer literals like in case of (assign x 5)
+        #handle integer literals like in case of (assign x 5)
         if ch.isdigit():
             start = i
             while i < len(source) and source[i].isdigit():
